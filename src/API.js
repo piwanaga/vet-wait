2c458407fd9efde2e209cb1977dc29f3faea09b1
phillip <phillip.iwanaga@gmail.com>
2022-01-13 12:41:38 -0800

    checking for errors on api call

--- src/API.js
+++ src/API.js
@@ -6,6 +6,7 @@ const KEY ='753f4a473e3773b284219c6a3c94828de43f199c8k3LNLnNfLQRsoes0stOdBGDn'
 const sendText = async (phone, ownerName, petName) => {
     const phoneStripped = phone.replace('-', '').replace('-', '')
     try {
+        console.log('URL:', BASE_URL, 'KEY:', KEY, 'phone:', phone, 'stripped:', phoneStripped, 'owner:', ownerName, 'pet:', petName)
         const res = await axios.post(BASE_URL, {
             phone: phoneStripped,
             message: `Hi ${ownerName}, ${petName} is ready to be checked in! Please come to the front desk in the next 10 muinutes to start your appointment. (This is a test message from VetWait)`,
