e0dd6ad164fc9d6bb584b3ef640d42ed25ac6525
phillip <phillip.iwanaga@gmail.com>
2022-01-13 12:35:22 -0800

    add error catch for api call

--- src/API.js
+++ src/API.js
@@ -5,12 +5,17 @@ const KEY ='753f4a473e3773b284219c6a3c94828de43f199c8k3LNLnNfLQRsoes0stOdBGDn'
 
 const sendText = async (phone, ownerName, petName) => {
     const phoneStripped = phone.replace('-', '').replace('-', '')
-    const res = await axios.post(BASE_URL, {
-        phone: phoneStripped,
-        message: `Hi ${ownerName}, ${petName} is ready to be checked in! Please come to the front desk in the next 10 muinutes to start your appointment. (This is a test message from VetWait)`,
-        key: KEY
-    });
-    return res
+    try {
+        const res = await axios.post(BASE_URL, {
+            phone: phoneStripped,
+            message: `Hi ${ownerName}, ${petName} is ready to be checked in! Please come to the front desk in the next 10 muinutes to start your appointment. (This is a test message from VetWait)`,
+            key: KEY
+        });
+        return res
+    } catch (e) {
+        console.error(e)
+    }
+    
 }
 
 export default sendText
