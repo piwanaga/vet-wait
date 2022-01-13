bcceb01996ce5762655a40c3d3d098e995f3ddf2
phillip <phillip.iwanaga@gmail.com>
2022-01-13 13:28:27 -0800

    added secrets.js

--- src/API.js
+++ src/API.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
+import { API_KEY } from './secrets';
 
 const BASE_URL = 'https://textbelt.com/text'
-const KEY ='753f4a473e3773b284219c6a3c94828de43f199c8k3LNLnNfLQRsoes0stOdBGDn'
 
 const sendText = async (phone, ownerName, petName) => {
     const phoneStripped = phone.replace('-', '').replace('-', '')
     try {
-        console.log('URL:', BASE_URL, 'KEY:', KEY, 'phone:', phone, 'stripped:', phoneStripped, 'owner:', ownerName, 'pet:', petName)
         const res = await axios.post(BASE_URL, {
             phone: phoneStripped,
             message: `Hi ${ownerName}, ${petName} is ready to be checked in! Please come to the front desk in the next 10 muinutes to start your appointment. (This is a test message from VetWait)`,
-            key: KEY
+            key: API_KEY
         });
         return res
     } catch (e) {
