45ff5ce01fd3af654dd890bb0a8ee20b6f971817
phillip <phillip.iwanaga@gmail.com>
2022-02-15 13:26:55 -0800

    improved table layout on small screen

--- src/Queue.js
+++ src/Queue.js
@@ -23,7 +23,7 @@ const Queue = ({ items, clearItems }) => {
         return textSent.includes(i.id) ? 
             <button
                 disabled
-                className= 'disabled text-blue-400 text-xs tracking-wider bg-blue-200 bg-opacity-20 px-2 py-1 sm:ml-4 md:ml-8 rounded-lg focus:outline-none focus:ring focus:ring-offset-2 focus:ring-gray-200 focus:ring-opacity-50 transform transition' 
+                className= 'disabled text-blue-400 text-xs tracking-wider bg-blue-200 bg-opacity-20 px-2 py-1 ml-2 sm:ml-4 md:ml-8 rounded-lg focus:outline-none focus:ring focus:ring-offset-2 focus:ring-gray-200 focus:ring-opacity-50 transform transition' 
                 > 
                 text sent
                 <span> 
@@ -33,7 +33,7 @@ const Queue = ({ items, clearItems }) => {
                 </span>
             </button> :
             <button
-                className= 'text-white bg-blue-400 text-xs tracking-wider px-2 py-1 sm:ml-4 md:ml-8 rounded-lg border hover:bg-blue-300  focus:outline-none focus:ring focus:ring-offset-2 focus:ring-blue-400 focus:ring-opacity-50 active:bg-blue-500 transform transition'
+                className= 'text-white bg-blue-400 text-xs tracking-wider px-2 py-1 ml-2 sm:ml-4 md:ml-8 rounded-lg border hover:bg-blue-300  focus:outline-none focus:ring focus:ring-offset-2 focus:ring-blue-400 focus:ring-opacity-50 active:bg-blue-500 transform transition'
                 onClick={() => handleSendText(i.phone, i.firstName, i.petName, i.id)}
                 > 
                 Send Text
@@ -71,11 +71,11 @@ const Queue = ({ items, clearItems }) => {
             <table className='table-fixed w-full border-collapse'>
                 <thead className='bg-gray-100'>
                     <tr className='border-b'>
-                        <th className='hidden sm:table-cell w-12 md:w-20 lg:w-16 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>Pos</th>
-                        <th className='w-20 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>Owner</th>
-                        <th className='w-16 lg:w-20 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>Pet</th>
-                        <th className='sm:w-36 md:w-40 lg:w-28 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>Phone</th>
-                        <th className='sm:w-32 md:w-28 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>
+                        <th className='hidden sm:table-cell w-10 md:w-20 lg:w-16 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>Pos</th>
+                        <th className='hidden sm:table-cell w-20 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>Owner</th>
+                        <th className='w-16 sm:w-14 lg:w-20 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>Pet</th>
+                        <th className='w-36 sm:w-36 md:w-40 lg:w-28 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>Phone</th>
+                        <th className='w-20 sm:w-24 md:w-28 text-left text-sm sm:text-lg font-semibold px-4 py-4 tracking-wide text-gray-900'>
                             Checked In
                         </th>
                     </tr>
@@ -84,13 +84,13 @@ const Queue = ({ items, clearItems }) => {
                 {items.map((i, idx) => (
                     <tr key={i.id} className='hover:bg-gray-50'>
                         <td className='hidden sm:table-cell border px-3 py-3 text-gray-900 text-xs sm:text-sm'>{idx + 1}</td>
-                        <td className='border px-3 py-3 text-gray-900 text-xs sm:text-sm overflow-auto'>{i.firstName} {i.lastName}</td>
+                        <td className='hidden sm:table-cell border px-3 py-3 text-gray-900 text-xs sm:text-sm overflow-auto'>{i.firstName} {i.lastName}</td>
                         <td className='border px-3 py-3 text-gray-900 text-xs sm:text-sm overflow-auto'>{i.petName}</td>
                         <td className='border px-3 py-3 text-gray-900 text-xs sm:text-sm text-center sm:text-left'>
                             {i.phone}   
                             {renderTextBtn(i)}
                         </td>
-                        <td className='border-b px-3 py-3'>
+                        <td className='border-b  py-3'>
                             <div className='flex justify-center items-center'>
                             {renderCheckInBtn(i.id)}
                             </div>
